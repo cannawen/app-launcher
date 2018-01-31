be88d1082e8f60dfbd41b75e02f97d541f5a7eca
Canna Wen <cannawen@gmail.com>
2018-01-31 10:54:09 +0800

    Initial Commit

---
+++ launcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  launcher
+//
+//  Created by Canna Wen on 2018-01-31.
+//  Copyright © 2018 Canna Wen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
